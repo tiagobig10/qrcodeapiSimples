63e0c9292dde330277cd55a1242ed27b1361d968
USER <tiagobig10@gmail.com>
2025-10-21 22:57:08 -0300

    20-10-25

--- src/main/java/com/qrcodeapi/qrcodeapi/services/impl/QrcodeServiceImpl.java
+++ src/main/java/com/qrcodeapi/qrcodeapi/services/impl/QrcodeServiceImpl.java
@@ -3,23 +3,46 @@ package com.qrcodeapi.qrcodeapi.services.impl;
 import com.qrcodeapi.qrcodeapi.responses.QrcodeResponse;
 import com.qrcodeapi.qrcodeapi.services.QrcodeService;
 import com.qrcodeapi.qrcodeapi.ultls.QrCode;
+import com.qrcodeapi.qrcodeapi.ultls.QrCodeLogger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QrcodeServiceImpl implements QrcodeService {
+
+    @Autowired
+    private QrCodeLogger logger;
+
     @Override
     public QrcodeResponse generate(String content, int size) {
 
-        if (size < 1) {
-            size = 1;
+        long startTime = System.currentTimeMillis();
+        long durationMs = 0;
+
+        try {
+            if (size < 1) {
+                size = 1;
+            }
+
+            QrCode qrCode = new QrCode();
+            String svg = qrCode.generateSvg(content, size, "");
+            QrcodeResponse response = new QrcodeResponse();
+            response.setSize(size);
+            response.setContent(svg);
+            return response;
+        } catch (Exception e) {
+
+            // log failure
+            durationMs = System.currentTimeMillis() - startTime;
+            logger.logFailure(content, e, durationMs);
+
+            throw new RuntimeException(e);
+        }finally {
+            // log success
+            durationMs = System.currentTimeMillis() - startTime;
+            logger.logSuccess(content,durationMs);
         }
 
-        QrCode qrCode = new QrCode();
-        String svg = qrCode.generateSvg(content, size, "");
-        QrcodeResponse response = new QrcodeResponse();
-        response.setSize(size);
-        response.setContent(svg);
-        return response;
     }
 
 
